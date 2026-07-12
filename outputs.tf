7c7c66d16f96a53d12399b06eed57298ee95286e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "virtual_machine_extensions_id" {
+  description = "Map of id values across all virtual_machine_extensions, keyed the same as var.virtual_machine_extensions"
+  value       = { for k, v in azurerm_virtual_machine_extension.virtual_machine_extensions : k => v.id }
+}
 output "virtual_machine_extensions_auto_upgrade_minor_version" {
   description = "Map of auto_upgrade_minor_version values across all virtual_machine_extensions, keyed the same as var.virtual_machine_extensions"
   value       = { for k, v in azurerm_virtual_machine_extension.virtual_machine_extensions : k => v.auto_upgrade_minor_version }
